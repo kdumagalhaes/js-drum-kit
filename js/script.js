5882d6c26273cfa140bf5cb14c4e39b53512bc55
kdumagalhaes <ce.magalhaespereira@gmail.com>
2020-07-19 12:15:51 -0300

    add new sounds

--- js/script.js
+++ js/script.js
@@ -1,7 +1,21 @@
 
-    window.addEventListener('keydown', playSound)
+    window.addEventListener('keydown', pushingBtn)
+    window.addEventListener('keyup', backToDefault)
+    window.addEventListener('keydown', playingSound)
 
-    function playSound(e) {
+    function pushingBtn(e) {
         const key = document.querySelector(`div[data-key="${e.keyCode}"]`)
+        key.style.cssText = "transform: scale(0.98); box-shadow: none;"
     }
 
+    function backToDefault(e) {
+        const key = document.querySelector(`div[data-key="${e.keyCode}"]`)
+        key.style.cssText = "transform: scale(1); box-shadow: 1px 1px 3px #99967c;"
+    }
+
+    function playingSound(e) {
+        const key = document.querySelector(`div[data-key="${e.keyCode}"]`)
+        const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+        audio.currentTime = 0;
+        audio.play();
+    }
